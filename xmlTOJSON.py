962ee16c3922fc8b42d95cd785b211faab70c0e6
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-13 17:03:21 +0800

    Update xmlTOJSON.py

--- xmlTOJSON.py
+++ xmlTOJSON.py
@@ -1,7 +1,11 @@
 import json
 import xmltodict
 import datetime
- 
+
+def formatDate():
+	dt = datetime.datetime.now()
+	return dt.strftime("%d/%m/%Y")
+
 with open("C:\\Users\\90499\\XMLtranslation\\strings.xml") as fd:
     XMLstring = fd.read()
 
@@ -9,10 +13,22 @@ rawJSON = xmltodict.parse(XMLstring)
 
 # insert formatting code here and initialize finalJSON
 
-names = []
+userName = input("Who are you: ")
 for objs in rawJSON["resources"]["string"]:
-  #Add the code here
+	objs["Screen_Name"] = objs["@name"]
+	del objs["@name"]
+	objs["Owner"] = "Sehun Park"
+	objs["OtherLanguages"] = [{'Contents': objs["#text"], 'Language': 'English', 'Editor': userName, 'Date': formatDate()}]
+	del objs["#text"]
 
+for objsArrs in rawJSON["resources"]["string-array"]:
+	objsArrs["Screen_Name"] = objsArrs["@name"]
+	del objsArrs["@name"]
+	objsArrs["Owner"] = "Sehun Park"
+	objsArrs["OtherLanguages"] = [{'Contents': objsArrs["item"], 'Language': 'English', 'Editor': userName, 'Date': formatDate()}]
+	del objsArrs["item"]
+
+	
 # End of formatting code
 
 finalJSONString = json.dumps(rawJSON, indent=4)
